5288c26faa8004e8d5764b11f9045e8fb968b610
kyuhyeongkwon <akcps1tp@naver.com>
2022-09-13 22:34:21 +0900

    solve: 자료구조 - BOJ 1158 (#2)

---
+++ kyuhyeong/boj/자료구조/후위표기식/solution.py
@@ -0,0 +1,31 @@
+import sys
+
+if __name__ == '__main__':
+    infix = sys.stdin.readline().strip()
+    postfix = ""
+    stack = []
+
+    for ch in infix:
+        if 'A' <= ch <= 'Z':
+            postfix += ch
+        elif ch in ['*', '/']:
+            if stack and stack[-1] in ['*', '/']:
+                postfix += stack.pop()
+            stack.append(ch)
+        elif ch in ['+', '-']:
+            while stack and stack[-1] in ['*', '/', '+', '-']:
+                postfix += stack.pop()
+            stack.append(ch)
+        elif ch == '(':
+            stack.append(ch)
+        else:
+            while True:
+                top = stack.pop()
+                if top == '(':
+                    break
+                postfix += top
+
+    while stack:
+        postfix += stack.pop()
+
+    print(postfix)
