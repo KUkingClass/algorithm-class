0ab4f5493e546a2510aac699a55b4deadedf53fc
mimwin <kms105903@naver.com>
2022-09-18 02:25:32 +0900

    solve: 자료구조 - BOJ 18258 큐 2 (#2)

---
+++ miseung/boj/data structure/18258/solution.py
@@ -0,0 +1,34 @@
+import sys
+from collections import deque
+
+queue = deque()
+n = int(sys.stdin.readline())
+
+for _ in range(n):
+    i = sys.stdin.readline().split()
+
+    if i[0] == 'push':
+        queue.append(int(i[1]))
+    elif i[0] == 'pop':
+        if not queue:
+            print(-1)
+        else:
+            print(queue[0])
+            queue.popleft()
+    elif i[0] == 'size':
+        print(len(queue))
+    elif i[0] == 'empty':
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+    elif i[0] == 'front':
+        if not queue:
+            print(-1)
+        else:
+            print(queue[0])
+    elif i[0] == 'back':
+        if not queue:
+            print(-1)
+        else:
+            print(queue[-1])
