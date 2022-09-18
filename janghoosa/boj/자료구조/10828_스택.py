0a86b20442cd15292414047af87a338aff2b5348
janghoosa <janghoosa@gmail.com>
2022-09-18 16:59:53 +0900

    solve: 자료구조 - BOJ 10828 스택 (#2)

---
+++ janghoosa/boj/자료구조/10828_스택.py
@@ -0,0 +1,26 @@
+# 10828 스택
+# https://www.acmicpc.net/problem/10828
+N = int(input())
+stack = []
+for _ in range(N):
+    action = input().split()
+    if action[0] == 'push':
+        stack.append(action[1])
+    elif action[0] == 'pop':
+        if stack:
+            print(stack.pop())
+        else:
+            print('-1')
+    elif action[0] == 'size':
+        print(len(stack))
+    elif action[0] == 'empty':
+        if stack:
+            print('0')
+        else:
+            print('1')
+    elif action[0] == 'top':
+        if stack:
+            print(stack[-1])
+        else:
+            print('-1')
+        
